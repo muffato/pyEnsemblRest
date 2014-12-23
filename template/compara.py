ceeb5da353cfbcff9fa65af1a34d09794c846ecc
Matthieu Muffato <muffato@ebi.ac.uk>
2014-12-23 01:45:50 +0000

    More methods for gene-tree nodes

--- template/compara.py
+++ template/compara.py
@@ -8,6 +8,18 @@ def _tax_parent(self):
         self.__dict__['_parent'] = copy.__dict__['_parent']
     return self.__dict__['_parent']
 
+def _tax_children(self):
+    if self.leaf:
+        return []
+    if '_children' not in self.__dict__:
+        if not hasattr(self, '_id'):
+            print("'id' is not defined, cannot fetch the parent node")
+            return []
+        copy = self.server.getTaxonomyEntryByID(self.id)
+        self.__dict__['_children'] = copy.__dict__['_children']
+    return self.__dict__['_children']
+
+
 
 #__GENERATED_OBJECTS__
 
@@ -24,7 +36,18 @@ def _gtn_get_all_leaves(self):
 
 setattr(GeneTreeNode, 'get_all_leaves', _gtn_get_all_leaves)
 
+def _gtn_get_all_nodes(self):
+    """
+        Get all the nodes in this sub-tree, including the root
+    """
+    if '_children' not in self.__dict__:
+        return [self]
+    l = [self]
+    for n in self.children:
+        l.extend(n.get_all_nodes())
+    return l
 
+setattr(GeneTreeNode, 'get_all_nodes', _gtn_get_all_nodes)
 
 def _gt_get_all_leaves(self):
     """
@@ -33,4 +56,12 @@ def _gt_get_all_leaves(self):
     return self.tree.get_all_leaves()
 
 setattr(GeneTree, 'get_all_leaves', _gt_get_all_leaves)
+
+def _gt_get_all_nodes(self):
+    """
+        Get all the nodes in this tree
+    """
+    return self.tree.get_all_nodes()
+
+setattr(GeneTree, 'get_all_nodes', _gt_get_all_nodes)
 
